3079e73b59755dda8a8c324c67810dd5db170196
Sandy2266-D <santhoshdurai2266@gmail.com>
2021-08-13 21:00:42 +0530

    1 file modified

--- src/Test2.js
+++ src/Test2.js
@@ -3,34 +3,56 @@ import './style.css';
 import Test3 from './Test3';
 import { CounterContext } from '../context';
 import { NameContext } from '../context';
+// export default function Test2() {
+//   return (
+//     <CounterContext.Consumer>
+//       {context => {
+//         return (
+//           <NameContext.Consumer>
+//             {nameContext => {
+//               return (
+//                 <>
+//                   <h1>Hello Test2!</h1>
+//                   <p> Name- {nameContext.name}</p>
+//                   <h4>Counter Value = {context.count}</h4>
+//                   <button onClick={context.increment}>Increment Test 2</button>
+//                   <br />
+//                   <br />
+//                   <button onClick={context.decrement}>Decrement Test 2</button>
+//                   <br />
+//                   <br />
+//                   <button onClick={context.reset}>Reset Test 2</button>
+//                   <NameContext.Provider value={{ name: 'Guvi' }}>
+//                     <Test3 />
+//                   </NameContext.Provider>
+//                 </>
+//               );
+//             }}
+//           </NameContext.Consumer>
+//         );
+//       }}
+//     </CounterContext.Consumer>
+//   );
+// }
+
 export default function Test2() {
+  const counterContext = React.useContext(CounterContext);
+  const nameContext = React.useContext(NameContext);
   return (
-    <CounterContext.Consumer>
-      {context => {
-        return (
-          <NameContext.Consumer>
-            {nameContext => {
-              return (
-                <>
-                  <h1>Hello Test2!</h1>
-                  <p> Name- {nameContext.name}</p>
-                  <h4>Counter Value = {context.count}</h4>
-                  <button onClick={context.increment}>Increment Test 2</button>
-                  <br />
-                  <br />
-                  <button onClick={context.decrement}>Decrement Test 2</button>
-                  <br />
-                  <br />
-                  <button onClick={context.reset}>Reset Test 2</button>
-                  <NameContext.Provider value={{ name: 'Guvi' }}>
-                    <Test3 />
-                  </NameContext.Provider>
-                </>
-              );
-            }}
-          </NameContext.Consumer>
-        );
-      }}
-    </CounterContext.Consumer>
+    <>
+      <h1>Hello Test2!</h1>
+      <p> Name- {nameContext.name}</p>
+      <h4>Counter Value = {context.count}</h4>
+      <button onClick={context.increment}>Increment Test 2</button>
+      <br />
+      <br />
+      <button onClick={context.decrement}>Decrement Test 2</button>
+      <br />
+      <br />
+      <button onClick={context.reset}>Reset Test 2</button>
+      <NameContext.Provider value={{ name: 'Guvi' }}>
+        <Test3 />
+      </NameContext.Provider>
+    </>
   );
 }
