f0f544837d5361d65dd2d9a2ad0edf7693ec9420
Sandy2266-D <santhoshdurai2266@gmail.com>
2021-08-13 20:52:43 +0530

    1 file modified

--- src/Test1.js
+++ src/Test1.js
@@ -3,47 +3,47 @@ import './style.css';
 import Test2 from './Test2';
 import { CounterContext } from '../context';
 
-// export default function Test1(props) {
-//   return (
-//     <CounterContext.Consumer>
-//       {context => {
-//         return (
-//           <>
-//             <h1>Hello Test1!</h1>
-//             <p>count ={context.count}</p>
-//             <button onClick={context.increment}>Increment Test 1</button>
-//             <br />
-//             <br />
-//             <button onClick={context.decrement}>Decrement Test 1</button>
-//             <br />
-//             <br />
-//             <button onClick={context.reset}>Reset Test 1</button>
-//             <Test2 />
-//           </>
-//         );
-//       }}
-//     </CounterContext.Consumer>
-//   );
-// }
-export default class Test1 extends React.Component {
-  // incrementFromTest1 = () => {
-  //   this.context.increment();
-  // };
-  static ContextType = CounterContext;
-  render() {
-    return (
-      <>
-        <h1>Hello Test1!</h1>
-        <p>count ={this.context.count}</p>
-        <button onClick={this.context.increment}>Increment Test 1</button>
-        <br />
-        <br />
-        <button onClick={this.context.decrement}>Decrement Test 1</button>
-        <br />
-        <br />
-        <button onClick={this.context.reset}>Reset Test 1</button>
-        <Test2 />
-      </>
-    );
-  }
+export default function Test1(props) {
+  return (
+    <CounterContext.Consumer>
+      {context => {
+        return (
+          <>
+            <h1>Hello Test1!</h1>
+            <p>count ={context.count}</p>
+            <button onClick={context.increment}>Increment Test 1</button>
+            <br />
+            <br />
+            <button onClick={context.decrement}>Decrement Test 1</button>
+            <br />
+            <br />
+            <button onClick={context.reset}>Reset Test 1</button>
+            <Test2 />
+          </>
+        );
+      }}
+    </CounterContext.Consumer>
+  );
 }
+// export default class Test1 extends React.Component {
+//   // incrementFromTest1 = () => {
+//   //   this.context.increment();
+//   // };
+//   static ContextType = CounterContext;
+//   render() {
+//     return (
+//       <>
+//         <h1>Hello Test1!</h1>
+//         <p>count ={this.context.count}</p>
+//         <button onClick={this.context.increment}>Increment Test 1</button>
+//         <br />
+//         <br />
+//         <button onClick={this.context.decrement}>Decrement Test 1</button>
+//         <br />
+//         <br />
+//         <button onClick={this.context.reset}>Reset Test 1</button>
+//         <Test2 />
+//       </>
+//     );
+//   }
+// }
